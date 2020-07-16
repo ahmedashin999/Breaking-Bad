6826a6cf9aeaca27b6714b16fa36c856471fce7d
Ashin <ahmedashin999@gmail.com>
2020-07-16 13:43:05 +0600

    Updates

---
+++ precache-manifest.01ef62f415a05eea10aca710d1d8f3d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdec6a856d4f7cd1d85c46ebc11ffadf",
+    "url": "/Breaking-Bad/index.html"
+  },
+  {
+    "revision": "93ec68819ef1a001bf91",
+    "url": "/Breaking-Bad/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c5cf4fe70ad800cfa6a5",
+    "url": "/Breaking-Bad/static/css/main.1ddaeb6a.chunk.css"
+  },
+  {
+    "revision": "93ec68819ef1a001bf91",
+    "url": "/Breaking-Bad/static/js/2.7f5f2880.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Breaking-Bad/static/js/2.7f5f2880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5cf4fe70ad800cfa6a5",
+    "url": "/Breaking-Bad/static/js/main.2c6a161b.chunk.js"
+  },
+  {
+    "revision": "a53daacb764170a2a39d",
+    "url": "/Breaking-Bad/static/js/runtime-main.fcc39abe.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Breaking-Bad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Breaking-Bad/static/media/spinner.235b9b3e.gif"
+  }
+]);
